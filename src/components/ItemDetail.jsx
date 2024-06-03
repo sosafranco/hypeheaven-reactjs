4231d6b9515888dc2d3df34fe066c13c2120ab9b
francososa <fm.sosa01@gmail.com>
2024-06-03 17:04:05 -0300

    se arreglaron las rutas del json

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ItemDetail = ({ titulo, precio, imagen }) => {
     return (
-        <div>
+        <div className='producto-detalles'>
             <img src={imagen} alt={titulo} />
             <p>{titulo}</p>
             <p>Precio: ${precio}</p>
